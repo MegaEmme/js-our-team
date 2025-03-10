e3ef9407a54a77bb458afc5733648cc43d695c0a
MegaEmme <mikmusco@gmail.com>
2025-03-10 17:38:07 +0100

    feat: add renderHTML and createHTMLElement functions (and comments)

--- script.js
+++ script.js
@@ -38,3 +38,69 @@ const teamMembers = [
 ];
 
 console.log(teamMembers);
+
+//elemento html card  da creare
+
+/* <div class="card">
+
+<img class="image" src="/img/female1.png" alt="female1">
+
+<div class="info">
+    <h2>Laura Rossi</h2>
+    <h3> Designer</h3>
+    <h3 class="mail">mail@gmail.com</h3>
+</div>
+
+</div>  */
+
+//Prossimi step: 
+//renderizzare la card
+//creare elemeto html
+//appenderlo al parente nell'html
+
+const parentElement = document.querySelector('.main');
+
+console.log(parentElement);
+
+renderHTML(parentElement, teamMembers);
+
+
+//creo le funzioni
+
+// la prima crea ed appende un elemento html ad un genitore per quanti sono gli elementi dell'oggetto preso in considerazione
+
+function renderHTML(parent, elements){
+  console.log(parent);
+  console.log(elements);
+
+  let items = '';
+
+  for(let i= 0; i<elements.length; i++){
+
+    let currentElement=elements[i];
+
+    console.log(currentElement);
+
+    items += createHTMLElement(currentElement);
+
+  }
+
+  parent.innerHTML=items;
+
+  console.log(parent);
+
+};
+
+//la seconda nello specifico crea un elemtento html ed è usata all'interno della prima
+
+function createHTMLElement(object){
+
+  return `<div class="card">
+            <img class="image" src="${object.img}" alt="female1">
+            <div class="info">
+              <h2>${object.name}</h2>
+              <h3>${object.role}</h3>
+              <h3 class="mail">${object.email}</h3>
+            </div>
+          </div>`
+};
